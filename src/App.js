b255fffb307014e5163ce801a26f6ef5d94dcfb2
HebaAlhamaydh <Eng_heba_hamaydh@hotmail.com>
2022-05-11 17:37:54 +0300

    m

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
-
+import { Routes, Route } from "react-router-dom";
 import Home from './components/home/Home';
+import Tour from './components/tours/tour/Tour';
 
 function App() {
   return (
+    <>
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/city/:id" element={<Tour/>}/>
+      </Routes>
       <Home/>
+      
+      </>
   );
 }
 
